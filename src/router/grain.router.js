86700ae9ba2c072bc666558ad7064f5fb08c371e
Anjul Singh <anjulsingh462@gmail.com>
2025-12-24 00:21:14 +0530

    add authmiddleware in create grain categories

--- src/router/grain.router.js
+++ src/router/grain.router.js
@@ -1,9 +1,10 @@
 import {Router} from "express";
 import {CreateGrainCategoies, getAllGrainCategories} from "../controller/grain.controller.js";
+import authMiddleware from "../middleware/authmiddleware.js";
 
 const router=Router();
 
-router.route("/creategraincategories").post(CreateGrainCategoies);
+router.route("/creategraincategories").post(authMiddleware,CreateGrainCategoies);
 
 router.route("/allgraincategories").get(getAllGrainCategories);
 
