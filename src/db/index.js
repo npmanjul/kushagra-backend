74e6fbace495755b09c005498331d7e2a3dd0838
Anjul Singh <anjulsingh462@gmail.com>
2025-12-26 14:34:15 +0530

    fix deployment issue

--- src/db/index.js
+++ src/db/index.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB=async()=>{
     try{
-        const connectionInstance =await mongoose.connect(`${process.env.ENVIRONMENT!=="production"?process.env.MONGODB_URI:process.env.MONGODB_URI_LOCAL}`);
+        const connectionInstance =await mongoose.connect(`${process.env.ENVIRONMENT==="production"?process.env.MONGODB_URI:process.env.MONGODB_URI_LOCAL}`);
         console.log(`\n MongoDB connected !! DB HOST : ${connectionInstance.connection.host}`)
     }catch(error){
         console.log("MONGODB connection FAILED",error);
