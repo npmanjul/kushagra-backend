18da3f6ddb5b22e2ae33444dac037cc25056a13e
Anjul Singh <anjulsingh462@gmail.com>
2026-01-26 18:21:28 +0530

    fix

--- src/controller/generatePDF.controller.js
+++ src/controller/generatePDF.controller.js
@@ -1,42 +1,6 @@
-import {
-  employeeProfileTemplate,
-  transactionInvoiceTemplate,
-  transactionReportTemplate,
-} from "../utils/PDFTemplates.js";
-
 
 const generatePDF = async (req, res) => {
   try {
-    // const data = req.body;
-    // const service = req.params.service;
-
-    // let templateHTML;
-
-    // if (service === "transactions") {
-    //   templateHTML = transactionReportTemplate(JSON.parse(data.data));
-    // } else if (service === "profile") {
-    //   templateHTML = employeeProfileTemplate(JSON.parse(data.data));
-    // } else if (service === "usertransaction") {
-    //   templateHTML = transactionInvoiceTemplate(JSON.parse(data.data));
-    // }
-    // const file = { content: templateHTML };
-
-    // const pdfBuffer = await html_to_pdf.generatePdf(file, {
-    //   format: "A4",
-    //   printBackground: true,
-    //   margin: {
-    //     top: "20mm",
-    //     bottom: "20mm",
-    //     left: "10mm",
-    //     right: "10mm",
-    //   },
-    // });
-
-    // res.setHeader("Content-Type", "application/pdf");
-    // res.setHeader("Content-Disposition", "attachment; filename=invoice.pdf");
-
-
-    // res.send(pdfBuffer);
     return res.status(200).json({
       success: true,
       message: "PDF generated successfully (mock response)",
